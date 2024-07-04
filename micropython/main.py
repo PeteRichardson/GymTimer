da309b2ec9ded442758775c54eeb10792f3c482e
Pete Richardson <pete@peterichardson.com>
2024-07-04 15:24:21 -0700

    reset start of count to 0 from 55 (which was a debug setting for clock mode)

--- micropython/main.py
+++ micropython/main.py
@@ -13,7 +13,7 @@ def random_color():
     return (g, r, b)
 
 counter = Display(270, 28, 8, "clock")    # clock mode means show 01:01 instead of 00:61
-SECS = 55
+SECS = 0
 while True :
     counter.show(SECS, 1, color = random_color() )
     time.sleep(1.0)
